297593b78097c75972d710aa874ef32a42317a32
zhengzhenhua <675117863@qq.com>
2021-12-12 15:37:59 +0800

    first commit

---
+++ tm-product/src/main/java/com/fbs/tm/tmproduct/TmProductApplication.java
@@ -0,0 +1,13 @@
+package com.fbs.tm.tmproduct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TmProductApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TmProductApplication.class, args);
+    }
+
+}
